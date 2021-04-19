8da8b418667ac48e06a2c1866dda39fc54a278e6
climb719 <mary.oldweiler@gmail.com>
2021-04-19 18:50:09 -0400

    note controller update

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -2,15 +2,25 @@ class NoteController < ApplicationController
     #seperation of concerns, inherit from App controller - specified in config.ru
     #inherit from AC use in conjuction with main controller we are going to run
     #inherits so can use same info from configure do block and inherit from Sinatra Base and access any helper methods 
-    get '/notes' do
+    get '/notes' do #READ get all notes
+        @notes = Note.all.order(params["topic"])  #accessing model from controller
+        #passing to view so can render for uswr
         erb :"notes/index"
     end
 
     get '/notes/new' do
         if !logged_in?
-            redirect '/login' #acts likea return, will leave method and won't get to rb or if logged in will skip redirect and go to erb
+            redirect '/login' #acts like a return, will leave method and won't get to rb or if logged in will skip redirect and go to erb
         end
             erb :"notes/new"
+    end
+
+    get '/notes/:id' do #READ  get one note
+        if !logged_in? 
+            redirect '/login'
+        end
+        @note = Note.find(params["id]"])
+        erb :"notes/show"
     end
 
 
@@ -26,5 +36,13 @@ class NoteController < ApplicationController
         redirect '/notes'
     end
 
+    get 'notes/:id/:edit' do #view a form to update 1 note
+    end
+
+    put '/notes/:id' do #update 1 note based on the edit form
+    end
+
+    delete 'notes/:id' do
+    end
 
 end
