ecd7fb74858a46eb180be350c2beb2803dd1f01d
climb719 <mary.oldweiler@gmail.com>
2021-04-20 13:22:15 -0400

    note controller and views updates

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -3,12 +3,12 @@ class NoteController < ApplicationController
     #inherit from AC use in conjuction with main controller we are going to run
     #inherits so can use same info from configure do block and inherit from Sinatra Base and access any helper methods 
     get '/notes' do #READ get all notes
-        @notes = Note.all.order(params["topic"])  #accessing model from controller
-        #passing to view so can render for uswr
+        @notes = Note.all  #accessing model from controller
+        #passing to view so can render for user
         erb :"notes/index"
     end
 
-    get '/notes/new' do
+    get '/notes/new' do #to CREATE a note
         if !logged_in?
             redirect '/login' #acts like a return, will leave method and won't get to rb or if logged in will skip redirect and go to erb
         end
@@ -19,12 +19,13 @@ class NoteController < ApplicationController
         if !logged_in? 
             redirect '/login'
         end
-        @note = Note.find(params["id]"])
+        @note = Note.find(params["id"])
         erb :"notes/show"
     end
 
 
     post '/notes' do
+        #CREATES a new note
         if !logged_in?
             redirect '/login' #acts likea return, will leave method and won't get to rb or if logged in will skip redirect and go to erb
         end #important user can't access if not logged in - protects routest, especailly routes where hit database/add to db
@@ -36,13 +37,26 @@ class NoteController < ApplicationController
         redirect '/notes'
     end
 
-    get 'notes/:id/:edit' do #view a form to update 1 note
+    get '/notes/:id/edit' do #view a form to update/edit 1 note
+        @note = Note.find(params["id"]) #first find note 
+        # this will display the edit view 
+        #id is way to specify what note the user want to see, it's a particular note
+        #don't know what id is with note untill saved to db
+        erb :"notes/edit"
     end
 
-    put '/notes/:id' do #update 1 note based on the edit form
+    put '/notes/:id' do #update 1 note and db based on the edit form
+        @note = Note.find(params["id"]) #id a param -key - comes from route or form. key value pair
+    binding.pry #to access something in a hash use hash beraket notation, need to first find the specif note
+    #when any individual note is need, first need to find the note (returns whole note object)
+    #then need to display it in a view 
+        @note.update(params["note"])
+    erb :"notes/#{@note.id}"
     end
 
-    delete 'notes/:id' do
+
+    delete '/notes/:id' do
+       @note =  Note.find(params["id]"])
     end
 
 end
