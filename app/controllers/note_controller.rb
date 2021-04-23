2ed91593d9a06f57776e4a9621a387f477b56b98
climb719 <mary.oldweiler@gmail.com>
2021-04-23 10:37:26 -0400

    note views success

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -3,8 +3,10 @@ class NoteController < ApplicationController
     #inherit from AC use in conjuction with main controller we are going to run
     #inherits so can use same info from configure do block and inherit from Sinatra Base and access any helper methods 
     get '/notes' do #READ get all notes
-        @notes = current_user.notes.order(:topic)
-        @topics = Topic.all.group(:name)
+        @notes = current_user.notes
+        @topics = current_user.topics.group(:name)
+        #binding.pry
+        #binding.pry
         #current_user.notes.order(:topic)
         #accessing model from controller
         #passing to view so can render for user
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,7 +4,7 @@ class Topic < ActiveRecord::Base
     has_many :users, through: :notes
 end
 
-#gives us instance merhods
+#gives instance merhods
 #.notes
 #.notes << (note_objects) Note.create(titel: " " )
 #.notes.build(pass in notes attributes that's associated with )
