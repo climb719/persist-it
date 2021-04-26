d7300a50f3f743145becb9ea5e7644c8c0774e54
climb719 <mary.oldweiler@gmail.com>
2021-04-26 11:37:01 -0400

    update to views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,16 +7,28 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, ENV['SESSION_SECRET']
+    #set :show_exceptions, false
+    #default setting show what the exeption is and what is happening to help developer, turn to false so error message works 
     #store in session who is logged in and a cookie on client side to store session and 
     #every request verify that their cookie matches session 
     #secret to make sure your server hasn't been tampered with #so can keep safe with .env file
     #session is a hash that can store information in so we can add information to it
   end
 
+  not_found do 
+    #different from a record not found, this is for if a rote is not found
+    status 404
+    erb :"users/error"
+  end
+    
   get "/" do
     erb :welcome
   end
-
+  
+  error ActiveRecord::RecordNotFound do
+    erb :"users/error"
+  end
+  #rescue for roecord not found telling routes when this is what to do with error not display error page, so redirect 
 
   helpers do #allows views to access these method, from view can accesss any methods in helpers so 
     #especially for accessing our view  and available in any controller 
