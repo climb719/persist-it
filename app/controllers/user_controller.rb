1c967d93c12888ab802795f8ad93dbde7486e0a6
climb719 <mary.oldweiler@gmail.com>
2021-04-28 16:09:58 -0400

    validation fix

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,12 +8,12 @@ class UserController < ApplicationController
     post "/signup" do
         #instantiate a user and make sure they signed up with valid data
         user = User.new(name: params["name"], email: params["email"], password: params["password"])
-        if user.name.blank? || user.password.blank? || User.find_by_email(params["email"])
+        if user.name.blank? || user.email.blank? || user.password.blank? || User.find_by_email(params["email"])
             redirect '/signup' #redirect if invalid login attempt 
         else
             user.save
             #if valid signup log them in/start a session
-            session[:user_id] = user.id #store their user id in the session and set it equal to the user_id, addkey value pairs to session 
+            session[:user_id] = user.id #store their user id in the session and set it equal to the user_id, add key value pairs to session 
             #because session is a hash, can add info to it
             #need to keep track on server side who is logged in and every request verifying our logged in user
             #sessions and cookies - where site asks cookies - storing data in your client side application so can verify user or keep track of what you clicked on 
